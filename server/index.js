4d5d61f8217561e5bc26261ed843ee7e2348af40
aicha-azr <aichaazeroual03@gmail.com>
2024-01-29 12:21:05 +0100

    routers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const router = require("./Routers/router");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+
 require("./Connection");
+app.use(router);
 app.listen(PORT, () => {
   console.log(`app listening at : http://localhost:${PORT}/`);
 });
