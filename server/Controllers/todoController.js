c710b74c65a4893d1dedce043282caf342f230d6
khassarMohamed <mohamedkhassar775@gmail.com>
2024-01-29 12:02:11 +0100

    method PUT and DELETE

--- server/Controllers/todoController.js
+++ server/Controllers/todoController.js
@@ -1,44 +1,51 @@
-const mongoose = require('mongoose');
-const Tache = require('../models/TodoModel')
+const TodoModel = require("../models/TodoModel");
 const Controller = {
-    postTask: async(req,res)=>{
-        try{
-            const result = await Tache.create({
-                title: req.title,
-                description: req.description,
-                status: req.status,
-                priority:req.priority,
-                created_by: req.created_by,
-                deadline:req.deadline
-            }
-            
-            )
-        }catch(e){
-            console.log(e.mesage)
-        }
-    },
-    getAllTasks : async (req,res)=>{
-        try{
-           const result = await Tache.find(); 
-           console.log(result)
-        }catch(e){
-            console.log(e.message)
-        } 
-        
-
-    },
-    getTaskByID :  async (req,res)=>{
-        try{
-           const result = await Tache.find(req.params.id); 
-           console.log(result)
-        }catch(e){
-            console.log(e.message)
-        } 
-        
-
+  postTask: async (req, res) => {
+    try {
+      const result = await TodoModel.create({
+        title: req.title,
+        description: req.description,
+        status: req.status,
+        priority: req.priority,
+        created_by: req.created_by,
+        deadline: req.deadline,
+      });
+    } catch (err) {
+      console.log(err.message);
     }
-
-
-}
+  },
+  getAllTasks: async (req, res) => {
+    try {
+      const result = await TodoModel.find();
+      console.log(result);
+    } catch (err) {
+      console.log(err.message);
+    }
+  },
+  getTaskByID: async (req, res) => {
+    try {
+      const result = await TodoModel.find(req.params.id);
+      console.log(result);
+    } catch (err) {
+      console.log(err.message);
+    }
+  },
+  updateTask: async (req, res) => {
+    try {
+      await TodoModel.findByIdAndUpdate(req.params.id, req.body);
+    } catch (err) {
+      console.log(err.message);
+    }
+  },
+  deleteTask: async (req, res) => {
+    try {
+      await TodoModel.findByIdAndUpdate(req.params.id, {
+        delete_at: new Date().toISOString(),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
 
 module.exports = Controller;
