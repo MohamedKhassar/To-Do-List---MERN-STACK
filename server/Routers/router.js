3f8ff257e001304d93e6e94e4e30ac54b142430f
aicha-azr <aichaazeroual03@gmail.com>
2024-01-29 12:19:30 +0100

    routers

--- server/Routers/router.js
+++ server/Routers/router.js
@@ -6,6 +6,6 @@ router.get('/api/tasks', Controller.getAllTasks)
 router.get('/api/tasks/:id', Controller.getTaskByID)
 router.post('/api/tasks', Controller.postTask)
 router.put('/api/tasks/:id', Controller.updateTask)
-router.delete('/api/tasks/:id', Controller.deleteTask)
+router.put('/api/delete/tasks/:id', Controller.deleteTask)
 
 module.exports = router;
