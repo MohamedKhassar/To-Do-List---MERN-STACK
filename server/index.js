d649e68895318ba4fc02c9347b6d3c486223dccc
khassarMohamed <mohamedkhassar775@gmail.com>
2024-01-29 09:32:39 +0100

    monorepo

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express()
-const port = 2000;
-require('./Connection');
-app.listen(port, ()=>{
-    console.log(`app listining at : http://localhost:${port}/`)
-}) 
+const express = require("express");
+const app = express();
+const port = 3000;
+require("./Connection");
+app.listen(port, () => {
+  console.log(`app listining at : http://localhost:${port}/`);
+});
