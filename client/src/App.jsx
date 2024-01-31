b39c04e0da2ef2783531e1ac20287822616628c1
khassarMohamed <mohamedkhassar775@gmail.com>
2024-01-31 10:00:05 +0100

    set all the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import Alert from "./components/design-system/Alert";
-
+import { useEffect } from "react"
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
+import Tasks from "./components/pages/Tasks"
+import Task from "./components/pages/Task"
+import UpdateTask from "./components/pages/UpdateTask"
 export default function App() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate("/tasks")
+  })
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/tasks" exact element={<Tasks />} />
+        <Route path="/task/:id" exact element={<Task />} />
+        <Route path="/update-task/:id" exact element={<UpdateTask />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
