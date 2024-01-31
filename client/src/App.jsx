6d8d6232043f0bed24faa98cb17b58eacbe7d37d
khassarMohamed <mohamedkhassar775@gmail.com>
2024-01-31 11:17:53 +0100

    fix the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,29 @@
-import { useEffect } from "react"
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom"
 import Tasks from "./components/pages/Tasks"
 import Task from "./components/pages/Task"
 import UpdateTask from "./components/pages/UpdateTask"
+import { Route, Routes, redirect, useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 export default function App() {
+  const location = useLocation();
   const navigate = useNavigate()
   useEffect(() => {
-    navigate("/tasks")
+    const NavigatePage = () => {
+      switch (location.pathname) {
+        case "/":
+          return navigate("/tasks")
+      }
+    }
+    NavigatePage()
   })
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/tasks" exact element={<Tasks />} />
-        <Route path="/task/:id" exact element={<Task />} />
-        <Route path="/update-task/:id" exact element={<UpdateTask />} />
-      </Routes>
-    </BrowserRouter>
+
+    <Routes>
+      < Route path="/tasks" exact element={< Tasks />} />
+      < Route path="/task/:id" exact element={< Task />} />
+      < Route path="/update-task/:id" exact element={< UpdateTask />} />
+    </Routes>
+
+
+
   )
 }
